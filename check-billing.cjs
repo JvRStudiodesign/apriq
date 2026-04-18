fc1e39a942d9e5aa270b1f0b79f1c78886c71157
JvRStudiodesign <jvrstudiodesign@gmail.com>
2026-04-18 11:22:03 +0200

    Cleanup: remove all fix scripts from root

--- check-billing.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('src/pages/BillingPage.jsx', 'utf8').split('\n');
-lines.slice(0, 30).forEach((l, i) => console.log(i+1, l));
--- check-layout.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('src/components/Layout.jsx', 'utf8').split('\n');
-// Print lines 245-275 to see the broken area
-lines.slice(244, 275).forEach((l, i) => console.log(i+245, l));
